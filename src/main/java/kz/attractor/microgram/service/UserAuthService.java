d02a28b4b380fb305a0aaaf3c37839edd6d28f3b
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-03-27 17:33:03 +0600

    create UserService and SecurityConfig

---
+++ src/main/java/kz/attractor/microgram/service/UserAuthService.java
@@ -0,0 +1,28 @@
+package kz.attractor.microgram.service;
+
+import kz.attractor.microgram.Model.User;
+import kz.attractor.microgram.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserAuthService implements UserDetailsService {
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<User> user = repo.findByUsername(s);
+        if (user.isPresent())
+            return user.get();
+
+        throw new UsernameNotFoundException("User does not exit");
+    }
+}
