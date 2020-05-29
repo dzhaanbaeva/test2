81b5d19df1d27a34d0ba945f14490f778ee70574
dzhaanbaeva <dzhaanbaeva@gmail.com>
2020-05-30 02:55:19 +0600

    lesson57.js change

--- src/main/resources/static/js/lesson57.js
+++ src/main/resources/static/js/lesson57.js
@@ -186,6 +186,7 @@ window.addEventListener('load', function () {
         const div = document.createElement('div');
         div.setAttribute("id", post.id);
         const h2 = document.createElement('h2');
+
         h2.innerHTML = post.user.name;
         const p = document.createElement("p");
         const div2 = document.createElement('div');
